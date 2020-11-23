83c874c30dec58e0fb45b552906d924ce018f70a
Sen Procheta <sprocheta@aneto1.computing.dcu.ie>
2020-11-23 16:23:59 +0000

    TopicFile Creation Code Added

--- TrustEvaluator/src/main/java/org/trusteval/evaluator/ComputeStatistics.java
+++ TrustEvaluator/src/main/java/org/trusteval/evaluator/ComputeStatistics.java
@@ -14,6 +14,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanQuery;
@@ -129,11 +131,11 @@ public class ComputeStatistics {
         BufferedReader br = new BufferedReader(fr);
         String line = br.readLine();
         ArrayList<QueryObject> tqs = new ArrayList<>();
-        
+
         FileWriter fw = new FileWriter(new File("C:/Users/Procheta/Downloads/negativeQid.txt"));
         BufferedWriter bw = new BufferedWriter(fw);
         HashMap<String, String> qMap = new HashMap<>();
-        
+
         while (line != null) {
             String st[] = line.split("\t");
             String qid = st[0];
@@ -150,7 +152,7 @@ public class ComputeStatistics {
             String q1 = st[0];
             String q2 = st[1];
             if (qMap.containsKey(q1) && qMap.containsKey(q2)) {
-                bw.write(String.valueOf(qMap.get(q1)) + "\t" + q1+"\t"+ String.valueOf(qMap.get(q2)) + "\t" + q2+ "\t"+st[2]);
+                bw.write(String.valueOf(qMap.get(q1)) + "\t" + q1 + "\t" + String.valueOf(qMap.get(q2)) + "\t" + q2 + "\t" + st[2]);
                 bw.newLine();
             } else {
                 char c = '"';
@@ -164,11 +166,54 @@ public class ComputeStatistics {
 
     }
 
+    public void getUniqueIds() throws FileNotFoundException, IOException {
+        FileReader fr = new FileReader(new File("C:\\Users\\Procheta\\Downloads\\result.txt_msmarco_one_label"));
+        BufferedReader br = new BufferedReader(fr);
+        String line = br.readLine();
+
+        HashSet<String> qids = new HashSet<>();
+
+        while (line != null) {
+            String st[] = line.split("\t");
+            qids.add(st[0]);
+            line = br.readLine();
+        }
+        //  System.out.println("The number of qids "+qids);
+
+        fr = new FileReader(new File("C:\\Users\\Procheta\\Downloads\\qrels.train.tsv"));
+        br = new BufferedReader(fr);
+        line = br.readLine();
+
+        HashSet<String> qrel = new HashSet<>();
+
+        while (line != null) {
+            String st[] = line.split("\t");
+            qrel.add(st[0]);
+            line = br.readLine();
+        }
+        // System.out.println(qrel);
+
+        Iterator it = qids.iterator();
+
+        int count = 0;
+        while (it.hasNext()) {
+            String st = (String) it.next();
+            if (!qrel.contains(st)) {
+                count++;
+                System.out.println(st);
+            }
+            
+
+        }
+        System.out.println(count);
+    }
+
     public static void main(String[] args) throws IOException {
         ComputeStatistics cmp = new ComputeStatistics();
         //cmp.computeStatistics();
         //cmp.seperateLabels();
         //cmp.computeqIDS();
-        cmp.prepareModifiedMSMArco();
+        //  cmp.prepareModifiedMSMArco();
+        cmp.getUniqueIds();
     }
 }
